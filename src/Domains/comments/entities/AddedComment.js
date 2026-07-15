bd294afa7632c33581d172d21960b6ed20fdbdee
Hermansyah <herman.piliang23@gmail.com>
2026-07-15 11:59:43 +0700

    refactor: threadId to thread_id in added comment domain

--- src/Domains/comments/entities/AddedComment.js
+++ src/Domains/comments/entities/AddedComment.js
@@ -2,20 +2,20 @@ class AddedComment {
   constructor(payload) {
     this._verifyPayload(payload);
 
-    const { id, owner, threadId, content } = payload;
+    const { id, owner, thread_id, content } = payload;
 
     this.id = id;
     this.owner = owner;
-    this.threadId = threadId;
+    this.thread_id = thread_id;
     this.content = content;
   }
 
-  _verifyPayload({ id, owner, threadId, content }) {
-    if (!id || !owner || !threadId || !content) {
+  _verifyPayload({ id, owner, thread_id, content }) {
+    if (!id || !owner || !thread_id || !content) {
       throw new Error('ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof id !== 'string' || typeof owner !== 'string' || typeof threadId !== 'string' || typeof content !== 'string') {
+    if (typeof id !== 'string' || typeof owner !== 'string' || typeof thread_id !== 'string' || typeof content !== 'string') {
       throw new Error('ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
