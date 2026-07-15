8989394205f0c1b2b2d0b6031b34f0f8e53658a5
Hermansyah <herman.piliang23@gmail.com>
2026-07-15 11:26:36 +0700

    feat: add comment table

---
+++ migrations/1784089469246_create-table-comments.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('comments', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        owner: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        thread_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        content: {
+            type: 'TEXT',
+            notNull: true,
+        },
+    });
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('comments');
+};
