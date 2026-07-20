6cb6ba576c361cdc043c3351383ce36acacfec0d
Hermansyah <herman.piliang23@gmail.com>
2026-07-20 23:43:37 +0700

    fix: failed LikeRepository test

--- src/Domains/likes/_test/LikeRepository.test.js
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -6,7 +6,7 @@ describe('LikeRepository interface', () => {
     const likeRepository = new LikeRepository();
 
     // Action & Assert
-    await expect(likeRepository.addLike('', '', '')).rejects.not.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.addLike('', '', '')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(likeRepository.deleteLike('', '', '')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(likeRepository.checkLikeAvailability('', '', '')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(likeRepository.getLikeCountsByThreadId('')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
